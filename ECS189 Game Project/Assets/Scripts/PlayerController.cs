735f16e5441e84b36428d692f78c1130ac274336
Margaret <mksit@ucdavis.edu>
2019-06-01 19:22:34 -0700

    implement shoot command with Fire1. only shoots horizontal to right

--- ECS189 Game Project/Assets/Scripts/PlayerController.cs
+++ ECS189 Game Project/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private IPlayerCommand Right;
     private IPlayerCommand Left;
     private IPlayerCommand Jump;
+    private IPlayerCommand Fire1;
 
     
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
         this.Right = ScriptableObject.CreateInstance<MovePlayerRightMovement>();
         this.Left = ScriptableObject.CreateInstance<MovePlayerLeftMovement>();
         this.Jump = ScriptableObject.CreateInstance<MovePlayerJumpMovement>();
+        this.Fire1 = new ShootCommand();
     }
 
     // Update is called once per frame
@@ -37,6 +39,10 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Move Jump");
             this.Jump.Execute(this.gameObject);
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            this.Fire1.Execute(this.gameObject);
         }
     }
 }
