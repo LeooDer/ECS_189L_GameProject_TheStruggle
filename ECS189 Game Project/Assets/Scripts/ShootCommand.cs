23b1598633e77d06615f519b6f3e831232082a6e
Margaret <mksit@ucdavis.edu>
2019-06-05 17:27:11 -0700

    change Projectile MonoBehavior and is destroy after 1 second

--- ECS189 Game Project/Assets/Scripts/ShootCommand.cs
+++ ECS189 Game Project/Assets/Scripts/ShootCommand.cs
@@ -6,23 +6,21 @@ using Player.Command;
 
 namespace Player.Command
 {
-    public class ShootCommand : ScriptableObject, IPlayerCommand
+    public class ShootCommand : MonoBehaviour //ScriptableObject, IPlayerCommand
     {
-        private static readonly Object ProjectilePrefab;
-        private static float SpeedFactor = 50.0f;
-
-        static ShootCommand()
+        
+        void Start()
         {
-            ProjectilePrefab = Resources.Load("prefabs/Projectile");
+            Destroy(gameObject, 1.0f);
         }
 
-        public void Execute(GameObject gameObject)
+        //Function will be called when this object hits an object with a collider
+        void OnCollisionEnter(Collision collision)
         {
-            var projectile = (GameObject)Instantiate(ProjectilePrefab, gameObject.transform.localPosition, gameObject.transform.rotation);
-            var projectileRigidBody = projectile.GetComponent<Rigidbody>();
-            projectileRigidBody.velocity = projectile.transform.right * SpeedFactor;
-
+            //Destroy this gameobject
+            Destroy(gameObject);
         }
+
     }
 }
 
