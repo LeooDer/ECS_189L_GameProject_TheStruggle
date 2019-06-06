cf28cb6db63df80d7304b47df1fed5b3b0cf6d34
LeooDer <lvty@ucdavis.edu>
2019-06-05 21:27:50 -0700

    HealthManager added(i dont know why i was overcomplicating it)

---
+++ ECS189 Game Project/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    private Dictionary<int, double> Health = new Dictionary<int, double>();
+
+    // Start is called before the first frame update
+    public void Add(int key,double health)
+    {
+        if(Health.ContainsKey(key) == false)
+        {
+            Health.Add(key, health);
+        }
+    }
+    
+    public bool Damaged(int key,double damage)
+    {
+        if (Health.ContainsKey(key))
+        {
+            double currentHealth = Health[key];
+            Debug.Log(currentHealth);
+            if ((currentHealth - damage) > 0)
+            {
+                Health[key] = currentHealth - damage;
+                return true;
+            }
+            else
+            {
+                Remove(key);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void Remove(int key)
+    {
+        Health.Remove(key); 
+    }
+
+}
