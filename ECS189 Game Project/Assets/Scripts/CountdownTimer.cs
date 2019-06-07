9b02fbf704b31c3783b7363f60bbf1b06f9f2d45
isaboi <34112422+isaboi@users.noreply.github.com>
2019-06-07 10:10:58 -0700

    Countdown timer finished.

--- ECS189 Game Project/Assets/Scripts/CountdownTimer.cs
+++ ECS189 Game Project/Assets/Scripts/CountdownTimer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,5 +23,49 @@ public class CountdownTimer : MonoBehaviour
         {
             //GameOver(); // Need to make a GameOver() function
         }
+    }
+}*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public Text countdownText;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Countdown");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("TIME LEFT: " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("Countdown");
+            countdownText.text = "TIMES UP!";
+            LoadLoseLevel();
+        }
+    }
+
+    IEnumerator Countdown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+
+    void LoadLoseLevel()
+    {
+        // Load the level named "Lose".
+        GameManager.Instance.ChangeScene("Lose");
     }
 }
