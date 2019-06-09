2562de1b8b5f11a12d1966a041df10fd135fdd88
LeooDer <lvty@ucdavis.edu>
2019-06-09 16:51:34 -0700

    Fall Detection and added some boss sounds

---
+++ ECS189 Game Project/Assets/Scripts/FallDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDetection : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player" || collider.gameObject.tag == "Enemy")
+        {
+            Destroy(collider.gameObject);
+            GameManager.Instance.ChangeScene("Lose");
+        }
+    }
+}
