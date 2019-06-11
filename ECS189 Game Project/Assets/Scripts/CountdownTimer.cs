fb106b38ed24bf6e7ae0907c8f546c4c994efe82
Isabel Kalogiannis <34112422+isaboi@users.noreply.github.com>
2019-06-10 20:53:20 -0700

    Update CountdownTimer.cs

--- ECS189 Game Project/Assets/Scripts/CountdownTimer.cs
+++ ECS189 Game Project/Assets/Scripts/CountdownTimer.cs
@@ -1,38 +1,10 @@
-﻿/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CountdownTimer : MonoBehaviour
-{
-    //[SerializeField] private 
-    private float timeLeft = 0f;
-    private readonly float startingTime = 90f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Game starts with 90 seconds.
-        timeLeft = startingTime;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        timeLeft -= 1 * Time.deltaTime;
-        if (timeLeft < 0)
-        {
-            //GameOver(); // Need to make a GameOver() function
-        }
-    }
-}*/
-
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public int timeLeft = 90;
+    public int timeLeft = 60;
     public Text countdownText;
 
     // Use this for initialization
